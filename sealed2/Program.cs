e6157003eeda7dbce3b3d75cb0ccda58c423a18e
Harshit01070143 <harshitsharma16971@gmail.com>
2023-09-05 08:50:43 +0530

    Add project files.

---
+++ sealed2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed2
+{
+    sealed class vehicles
+    {
+        public void startEngine()
+        {
+            Console.WriteLine("Car Engine is start ");
+        }
+        public void StopEngine()
+        {
+            Console.WriteLine("Car Engine is stop ");
+        }
+    }
+    class call
+    {
+        public static void Main(string[] args)
+        {
+
+            vehicles v = new vehicles();
+            v.startEngine();
+            Console.ReadKey();
+        }
+    }
+}
